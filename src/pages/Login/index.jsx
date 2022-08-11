690738c9947966fc88890594f7905c69a11f51c6
André <andremessias.az@gmail.com>
2022-08-10 23:49:31 -0300

    setup global styles

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
+import { Container } from "../../styles/GlobalStyles";
 import { Title, Paragrafo } from "./styled";
 
 export function Login() {
   return (
-    <>
+    <Container>
       <Title isRed={false}>
         Login
         <small>Oie</small>
       </Title>
       <Paragrafo>VAMOS TODOS NOS DIVERTIR!</Paragrafo>
-    </>
+      <h1>TESTE</h1>
+    </Container>
   );
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,3 +1,36 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle``;
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Roboto;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export const Container = styled.section`
+  max-width: 800px;
+  background-color: #0f0d0e;
+  margin: 30px auto;
+`;
