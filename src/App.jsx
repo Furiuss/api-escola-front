24c6236ce85c201e32cd01aa1a4d79b2d1418cad
André <andremessias.az@gmail.com>
2022-08-10 23:16:25 -0300

    add: add styled components and fix index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { Login } from "./pages/Login";
+
 function App() {
   return (
     <div className="app">
       <h1>Hello World</h1>
+      <Login />
     </div>
   );
 }
