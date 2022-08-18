f0ea920ef686308c1c618eb55b262e93e82dbbda
André <andremessias.az@gmail.com>
2022-08-17 21:15:52 -0300

    add: add new pages and remove another ones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import { Nav } from './styled';
 
 export function Header() {
-  toast.success('Deu certo cria!');
-  toast.error('ih, erro na matrix!');
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/user">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
       <Link to="/login">
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Alunos from './pages/Alunos';
 import Page404 from './pages/Page404';
 import PrivateRoute from './routes/PrivateRoute';
+import Register from './pages/Register';
+import Fotos from './pages/Fotos';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route element={<App />}>
         <Route path="/" element={<PrivateRoute />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Alunos />} />
         </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/fotos" element={<Fotos />} />
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
