6506ab7ebaddc770acb45af28d4199508c1782b7
André <andremessias.az@gmail.com>
2022-08-18 17:25:05 -0300

    setup: setup alunos route

--- src/pages/Alunos/index.jsx
+++ src/pages/Alunos/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-
+import { get } from 'lodash';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 import { Container } from '../../styles/GlobalStyles';
-import { AlunoContainer } from './styled';
+import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
 export default function Alunos() {
@@ -22,12 +24,25 @@ export default function Alunos() {
       <AlunoContainer>
         {alunos.map((aluno) => (
           <div key={String(aluno.id)}>
-            {aluno.nome} <br />
-            <img
-              crossOrigin=""
-              src={aluno.Fotos[0].url || ''}
-              alt={aluno.name}
-            />
+            <ProfilePicture>
+              {get(aluno, 'Fotos[0].url', false) ? (
+                <img
+                  crossOrigin=""
+                  src={aluno.Fotos[0].url || ''}
+                  alt={aluno.name}
+                />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+            <Link to={`/aluno/${aluno.id}/edit`}>
+              <FaEdit style={{ color: 'green' }} size={18} />
+            </Link>
+            <Link to="/">
+              <FaWindowClose style={{ color: 'red' }} size={18} />
+            </Link>
           </div>
         ))}
       </AlunoContainer>
--- src/pages/Alunos/styled.js
+++ src/pages/Alunos/styled.js
@@ -1,3 +1,34 @@
 import styled from 'styled-components';
 
-export const AlunoContainer = styled.div``;
+export const AlunoContainer = styled.div`
+  margin-top: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: 1px solid #eee;
+  }
+
+  a {
+    cursor: pointer;
+    transform: scale(0.9);
+    transition: 0.15s;
+  }
+
+  a:hover {
+    transform: scale(1);
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+`;
